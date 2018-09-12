59b48f483bed6d72df4872395d0184c9d44cda9c
Cenobio Moisés Vázquez Reyes <42991834+spidermoy@users.noreply.github.com>
2018-09-11 19:57:09 -0500

    Add files via upload
    Se quitó el módulo binarios y se incluyó su contenido en el módulo RandomKS

--- ParserNuXmv.hs
+++ ParserNuXmv.hs
@@ -1,7 +1,7 @@
 module ParserNuXmv where
 import Core
 import ParserForms
-import Binarios
+
 
 write_nuxmvLTL::KripkeS->[State]->[State]->[String]->[PathF]->Int->[Int]->IO ()
 write_nuxmvLTL ks@(KS (nstates,r,l)) states init vars forms lforms [ranInit,ranNumInit,ranKS,ranF] = 
@@ -24,23 +24,5 @@ write_nuxmvCTL ks@(KS (nstates,r,l)) states init vars forms lforms [ranInit,ranN
                           "TRANS\n"++let g = \sts -> case sts of {[s] -> "s"++show s;s':ss -> "s"++show s'++"|"++g ss} in let f = \sts -> case sts of {[s] -> "(s"++show s++" & next("++ (g . r) s ++"))\n\n";s':ss -> "(s"++show s'++" & next("++ (g . r) s' ++")) |\n"++ f ss} in f states++
                           stateFormsToFile forms)
                          
-                          
-{-                     
-write_nuxmvLTL::KripkeS->[State]->[State]->[String]->[PathF]->Int->[Int]->IO ()
-write_nuxmvLTL ks@(KS (nstates,r,l)) states init vars forms lforms [ranInit,ranNumInit,ranKS,ranF] = 
-                          writeFile "/home/moy/nuXmv-1.1.1-Linux/bin/ejemplo_random.smv" (("-- Semilla init: " ++ show ranInit)++("\n-- Semilla NumInit: " ++ show ranNumInit) ++ ("\n-- Semilla KS: " ++ show ranKS) ++ ("\n-- Semilla F: " ++ show ranF)++("\n-- Longitud fórmulas: "++show lforms)++"\n\n\n"++
-                          "MODULE main\n\n" ++
-                          "VAR\n\tstate: {" ++ let f = \vs -> case vs of {[s] -> "s"++show s;w:ws -> "s"++show w++","++f ws} in f states ++ "};\n\n" ++
-                          let f_init = \ss -> case ss of {[s] -> "s"++show s; t:ts -> "s"++show t ++ "," ++ f_init ts }  in "ASSIGN\n\tinit(state) := {"++ f_init init ++"};\n\tnext(state) := case\n"++ let g = \sts -> case sts of {[s] -> "s"++show s;s:ss -> "s"++show s++","++g ss} in let f = \sts -> case sts of {[] -> "\tesac;\n\n";s:ss -> "\t\tstate = s"++show s++" : {"++g (r s) ++"};\n"++ f ss} in f states ++                                        
-                          "DEFINE\n"++let g = \sts -> case sts of {[] -> "FALSE;\n";[s] -> "state = s"++show s++";\n";s:ss -> "state = s"++show s++" | "++g ss} in let f = \vs -> case vs of {[] -> "\n\n";v:vs -> let ss = [s | s<-states, l s v] in "\t"++v++" := "++g ss++f vs} in f vars ++                      
-                          pathFormsToFile forms) 
-                              
-
-write_nuxmvCTL::KripkeS->[State]->[State]->[String]->[StateF]->Int->[Int]->IO ()
-write_nuxmvCTL ks@(KS (nstates,r,l)) states init vars forms lforms [ranInit,ranNumInit,ranKS,ranF] = 
-                          writeFile "/home/moy/nuXmv-1.1.1-Linux/bin/ejemplo_random.smv" (("-- Semilla init: " ++ show ranInit)++("\n-- Semilla NumInit: " ++ show ranNumInit) ++ ("\n-- Semilla KS: " ++ show ranKS) ++ ("\n-- Semilla F: " ++ show ranF)++("\n-- Longitud fórmulas: "++show lforms)++"\n\n\n"++
-                          "MODULE main\n\n" ++
-                          "VAR\n\tstate: {" ++ let f = \vs -> case vs of {[s] -> "s"++show s;w:ws -> "s"++show w++","++f ws} in f states ++ "};\n\n" ++
-                          let f_init = \ss -> case ss of {[s] -> "s"++show s; t:ts -> "s"++show t ++ "," ++ f_init ts }  in "ASSIGN\n\tinit(state) := {"++ f_init init ++"};\n\tnext(state) := case\n"++ let g = \sts -> case sts of {[s] -> "s"++show s;s:ss -> "s"++show s++","++g ss} in let f = \sts -> case sts of {[] -> "\tesac;\n\n";s:ss -> "\t\tstate = s"++show s++" : {"++g (r s) ++"};\n"++ f ss} in f states ++                                        
-                          "DEFINE\n"++let g = \sts -> case sts of {[] -> "FALSE;\n";[s] -> "state = s"++show s++";\n";s:ss -> "state = s"++show s++" | "++g ss} in let f = \vs -> case vs of {[] -> "\n\n";v:vs -> let ss = [s | s<-states, l s v] in "\t"++v++" := "++g ss++f vs} in f vars ++                      
-                          stateFormsToFile forms) -}
+ 
+ 
--- RandomKS.hs
+++ RandomKS.hs
@@ -2,7 +2,6 @@ module RandomKS where
 import Core
 import Data.List(nub)
 import System.Random(mkStdGen,randomR,randomRs)
-import Binarios
 
 
 --Esta función genera un modelo aleatorio con 2^n estados a partir de una lista de semillas.
@@ -15,4 +14,21 @@ randomKS n rs = KS (2^n-1,r_random,l_random) where
                              let bss = statesbin n in 
                              (bss !! s) !! (read $ tail $ pj) 
                                       
-                                      
+sucB::[Bool]->[Bool]
+sucB [] = [True]
+sucB (False:bs) = True:bs 
+sucB (True:bs) = False : sucB bs
+
+
+bins::[[Bool]]
+bins = [False] : nexts bins 
+           where nexts (b:bs) = (sucB b) : nexts bs 
+           
+           
+completaCeros::Int->[Bool]->[Bool]
+completaCeros n bs = let m = length bs in
+                     if m < n then bs++(replicate (n-m) False) else bs  
+                     
+                     
+statesbin::Int->[[Bool]]
+statesbin n = (map (completaCeros n) $ take (2^n) bins)
